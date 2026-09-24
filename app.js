eed7994a13eb7d5541dee18bb0175f3666586ea8
sachin_sagar <sachin2010sagar@gmail.com>
2026-09-24 19:02:27 +0530

    Use environment port for deployment

--- app.js
+++ app.js
@@ -79,6 +79,8 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("errors.ejs",{message});
   //  res.status(statusCode).send(message);
 });
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
